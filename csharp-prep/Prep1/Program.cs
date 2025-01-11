c8df7a426899ad820ba4bcc83b3db7acc4fb5935
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-01-11 16:31:20 -0700

    program --> jbond name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,9 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+Console.Write("What is your first namme? ");
+string firstname = Console.ReadLine();
+
+Console.Write("What is your last name? ");
+string lastname = Console.ReadLine();
+
+Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
