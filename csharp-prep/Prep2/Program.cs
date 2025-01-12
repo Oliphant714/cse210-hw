6e93dae19572c36a11a1804db2225ce8533bc716
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-01-11 17:05:17 -0700

    Prep2 update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
-
-class Program
-{
+class Program {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your final percentage? ");
+        int gradeNumber = int.Parse(Console.ReadLine());
+        
+        
+        string gradeLetter = "Balderdash";
+
+        if (gradeNumber >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNumber >= 80 && gradeNumber < 90)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70 && gradeNumber < 80)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60 && gradeNumber < 70)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your final grade is {gradeLetter}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.Write("You passed.");
+        }
+        else{
+            Console.Write("You failed.");
+        }
+
+
+
     }
+
 }
