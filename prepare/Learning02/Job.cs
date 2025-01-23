d7eb04e0e1949b5128c69ff3fab9780d434666a0
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-01-22 20:49:04 -0700

    created job and resume files

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,10 @@
+//Keeps track of the company, job title, start year, and end year.
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    
+}
