9bb6b3b3998655690f2b5d010cbd14a9a7e6c822
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-02-06 13:00:46 -0700

    created samples from assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,22 +8,16 @@ class Program
         Console.WriteLine(f1.WrittenFraction());
         Console.WriteLine(f1.FractionDecimal());
 
-        Fraction f2 = new Fraction(6);
+        Fraction f2 = new Fraction(5);
         Console.WriteLine(f2.WrittenFraction());
         Console.WriteLine(f2.FractionDecimal());
 
-        Fraction f3 = new Fraction(6,7);
+        Fraction f3 = new Fraction(3,4);
         Console.WriteLine(f3.WrittenFraction());
         Console.WriteLine(f3.FractionDecimal());
 
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.WrittenFraction());
+        Console.WriteLine(f4.FractionDecimal());
     }
-
-
-
-
-
-
-
-
-
 }
