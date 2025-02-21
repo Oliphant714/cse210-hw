98742d91a5a81d3ec0c41c1687848c24e22ea3a5
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-02-20 17:38:28 -0700

    wrote project

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment firstStudent = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(firstStudent.GetSummary());
+
+        MathAssignment secondStudent = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(secondStudent.GetSummary());
+        Console.WriteLine(secondStudent.GetHomeworkList());
+
+        WritingAssignment thirdStudent = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(thirdStudent.GetSummary());
+        Console.WriteLine(thirdStudent.GetWritingInformation());
     }
 }
